01b19ddf1008e96462bc92d718631ce55e8d0597
Liza Vavrichyna <vavrichynaliza@gmail.com>
2023-06-06 11:15:13 -0500

    broken links and gifs added

--- scripts/database.js
+++ scripts/database.js
@@ -3,25 +3,29 @@ const database = [
         id: 1,
         name: "Nashville Soccer Club vs Manchester United",
         date: "July 4, 2023",
-        location: "GEODIS Park"
+        location: "GEODIS Park",
+        link: "http://victorian.fortunecity.com/brambles/4/frames/"
     },
     {
         id: 2,
         name: "Taylor Swift concert",
         date: "July 4, 2023",
-        location: "Nissan Stadium"
+        location: "Nissan Stadium",
+        link: "https://www.ticketmaster.com/lil-wayne-tickets/artist/712214"
     },
     {
         id: 3,
         name: "Musicians Corner",
         date: "July 4, 2023",
-        location: "Memphis"
+        location: "Memphis",
+        link: "https://nashvilleguru.com/33631/musicians-corner"
     }, 
     {
         id: 4,
         name: "CMA Fest",
         date: "July 4, 2023",
-        location: "Downtown Nashville"
+        location: "Downtown Nashville",
+        link: "https://www.tennesseetitans.com/tickets/"
     }
 ]
 
